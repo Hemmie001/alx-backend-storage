2988b051f8cfb467c95289ebd96d369c0179be73
Hemmie001 <emmanuelakej@gmail.com>
2024-01-18 23:12:53 +0000

    the resr correction

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,4 +1,7 @@
--- Write a SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
+-- Write a SQL script that creates a function
+-- SafeDiv that divides (and returns) the
+-- first by the second number or returns 0
+-- if the second number is equal to 0.
 --
 -- Requirements:
 --
--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,4 +1,6 @@
--- Write a SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students.
+-- Write a SQL script that creates a stored procedure
+-- ComputeAverageWeightedScoreForUsers that computes and store
+-- the average weighted score for all students.
 --
 -- Requirements:
 -- 
